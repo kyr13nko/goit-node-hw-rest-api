4563f034a03adc3cd6597585c74b10ba0f99375a
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2023-12-08 14:44:09 +0200

    fix authenticate

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -16,7 +16,7 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
 
-    if (!user || !user.token || user.token === token) next(HttpError(401));
+    if (!user || !user.token || user.token !== token) next(HttpError(401));
 
     req.user = user;
     next();
